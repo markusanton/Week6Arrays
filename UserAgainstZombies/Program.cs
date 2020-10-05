fd586463e9a953db5eb08c446e895b86d7d1bab4
markusanton <markusa070@gmail.com>
2020-10-05 14:45:27 +0300

    zombies

---
+++ UserAgainstZombies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAgainstZombies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userBag = new string[5];
+
+            int i = 0;
+            while (i < userBag.Length)
+            { 
+            Console.WriteLine("What items are in your bag?");
+            userBag[i] = Console.ReadLine();
+            i++;
+            }
+            Random rnd = new Random;
+            Console.WriteLine($"In case of a zombie attack you will have to fight off zombies with {userBag[rnd.Next(0, userBag.Length)]}");
+
+        }
+    }
+}
