22369df67ebc4c2d4a08a167504c98e82d29744b
markusanton <markusa070@gmail.com>
2020-10-05 14:23:53 +0300

    Add project files.

---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Week6Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] predictions = { "will win a million", "lose your smartphone", "fall in love", "buy youtube premium", "get a cookie" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, predictions.Length + 1);
+            Console.WriteLine($"Today you will {predictions[randomIndex]}");
+        }
+    }
+}
